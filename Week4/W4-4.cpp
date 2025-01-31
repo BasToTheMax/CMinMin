1feb25fd68200a0651271b8fcdc0c8de9ef7d1ad
BasToTheMax <info@bastothemax.nl>
2025-01-31 13:31:02 +0100

    W4 opdr 4

---
+++ Week4/W4-4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    char word[] = {'c', 'h', 'a', 'm', 'p', 'i', 'o', 'n', 's', 'h', 'i', 'p'};
+
+    cout << word[0] << endl;
+    cout << word[4] << endl;
+    cout << word[sizeof(word)-1] << endl;
+
+    return 0;
+
+}
