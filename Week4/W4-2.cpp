fa4a13861d330283323458b415231b506066057d
BasToTheMax <info@bastothemax.nl>
2025-01-31 13:21:52 +0100

    Opdracht 2

--- Week4/W4-2.cpp
+++ Week4/W4-2.cpp
@@ -3,5 +3,12 @@
 using namespace std;
 
 int main() {
+
+    char alphabet[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+    for (int i = 0; i < 26; i++) {
+        cout << alphabet[i] << endl;
+
+    }
     return 0;
 }
